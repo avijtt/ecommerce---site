a9feea98cee224e6d95946cd7db769e505746e66
avijtt <infoavi9090@gmail.com>
2024-04-22 14:43:44 +0545

    add strike and sale badge

--- e-com/store/models.py
+++ e-com/store/models.py
@@ -32,6 +32,9 @@ class Product(models.Model):
 	category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
 	description = models.TextField(max_length=1000, default='', blank=True)
 	image = models.ImageField(upload_to='upload/products/')
+	# Add sales 
+	is_sale = models.BooleanField(default=False)
+	sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=7)
 
 	def   __str__(self):
 		return self.name
