b95b4db078a0cc5e9fc64cda82ea2552b21d7467
avijtt <infoavi9090@gmail.com>
2024-04-25 21:28:44 +0545

    aadded cart app and urls route for cart app

---
+++ e-com/cart/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def cart_summery(request):
+    return render(request, 'cart/cart_summery.html', {})
+
+
+def cart_add(request):
+    pass
+
+def cart_delete(request):
+    pass
+
+def cart_update(request):
+    pass
+
