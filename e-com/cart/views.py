34a195d7cb8567c094245aff080e15c79f843bb1
avijtt <infoavi9090@gmail.com>
2024-04-26 23:35:28 +0545

    tried to add in cart by keeping session to all page

--- e-com/cart/views.py
+++ e-com/cart/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
 
 # Create your views here.
 def cart_summery(request):
@@ -6,7 +10,22 @@ def cart_summery(request):
 
 
 def cart_add(request):
-    pass
+    # get the cart
+    cart = Cart(request)
+    # test for post
+    if request.POST.get('action') == 'post':
+        # get product-id from jquery
+        product_id = int(request.POST.get('product_id'))
+        
+        # lookup product
+        product = get_object_or_404(Product, id = product_id)
+
+        # save the session
+        cart.add(product = product)
+
+        # return response
+        response = JsonResponse({'product name': product.name})
+        return response
 
 def cart_delete(request):
     pass
