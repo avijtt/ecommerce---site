f335ebcd54145d59ef793a466d41e68c87cb0094
avijtt <infoavi9090@gmail.com>
2024-05-04 09:43:04 +0545

    added Profilr model to extend with user

--- e-com/store/models.py
+++ e-com/store/models.py
@@ -1,7 +1,33 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+# create customer profile\
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	date_modified =  models.DateTimeField(User,auto_now=True)
+	phone = models.CharField(max_length=15, blank=True )
+	address1 = models.CharField(max_length=50, blank=True)
+	address2 = models.CharField(max_length=50, blank=True)
+	city = models.CharField(max_length=50, blank=True)
+	state = models.CharField(max_length=50, blank=True)
+	zipcode = models.CharField(max_length=50, blank=True)
+	cocuntry = models.CharField(max_length=50, blank=True)
+
+	def __str__(self) -> str:
+		return self.user.username
+
+# create profile when user sign up
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		user_profile = Profile(user=instance)
+		user_profile.save()
+# automate the profile
+post_save.connect(create_profile, sender=User)
+
+
 class Category(models.Model):
 	name= models.CharField(max_length=100)
 
