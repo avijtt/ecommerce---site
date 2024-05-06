67e21fdd3a7851793f1842f5414f83a935274745
avijtt <infoavi9090@gmail.com>
2024-05-06 12:01:38 +0545

    no change in model

--- e-com/store/models.py
+++ e-com/store/models.py
@@ -4,28 +4,34 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
-# create customer profile\
+
+# Create Customer Profile
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	date_modified =  models.DateTimeField(User,auto_now=True)
-	phone = models.CharField(max_length=15, blank=True )
-	address1 = models.CharField(max_length=50, blank=True)
-	address2 = models.CharField(max_length=50, blank=True)
-	city = models.CharField(max_length=50, blank=True)
-	state = models.CharField(max_length=50, blank=True)
-	zipcode = models.CharField(max_length=50, blank=True)
-	cocuntry = models.CharField(max_length=50, blank=True)
+	date_modified = models.DateTimeField(User, auto_now=True)
+	phone = models.CharField(max_length=20, blank=True)
+	address1 = models.CharField(max_length=200, blank=True)
+	address2 = models.CharField(max_length=200, blank=True)
+	city = models.CharField(max_length=200, blank=True)
+	state = models.CharField(max_length=200, blank=True)
+	zipcode = models.CharField(max_length=200, blank=True)
+	country = models.CharField(max_length=200, blank=True)
+	# old_cart = models.CharField(max_length=200, blank=True, null=True)
 
-	def __str__(self) -> str:
+	def __str__(self):
 		return self.user.username
 
-# create profile when user sign up
+# Create a user Profile by default when user signs up
 def create_profile(sender, instance, created, **kwargs):
 	if created:
 		user_profile = Profile(user=instance)
 		user_profile.save()
-# automate the profile
+
+# Automate the profile thing
 post_save.connect(create_profile, sender=User)
+
+
+
 
 
 class Category(models.Model):
