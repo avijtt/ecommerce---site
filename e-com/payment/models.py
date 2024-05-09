b07225ba9a523974c7fe20cf89f34f2512a59ad0
avijtt <infoavi9090@gmail.com>
2024-05-09 22:31:13 +0545

    add model in payment app

--- e-com/payment/models.py
+++ e-com/payment/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=100, )
+    email = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30, blank=True,null=True)
+    zipcode = models.CharField(max_length=10, blank=True,null=True)
+    country = models.CharField(max_length=64)
+    
+    # dont pluralize address
+    class Meta:
+        verbose_name_plural = "Shipping Address"
+
+    def __str__(self):
+        return f'Shipping Address - {str(self.id)}' 
