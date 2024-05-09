b9a4bfe84693b1819b8b16bf65463579063d5cb0
avijtt <infoavi9090@gmail.com>
2024-05-09 23:07:44 +0545

    field

--- e-com/payment/forms.py
+++ e-com/payment/forms.py
@@ -4,7 +4,8 @@ from .models import ShippingAddress
 class ShippingForm(forms.ModelForm):
 	shipping_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}), required=True)
 	shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=True)
-	shipping_address = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address'}), required=True)
+	shipping_address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address'}), required=True)
+	shipping_address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address'}), required=False)
 	shipping_city = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}), required=True)
 	shipping_state = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'State'}), required=False)
 	shipping_zipcode = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Zipcode'}), required=False)
@@ -12,6 +13,6 @@ class ShippingForm(forms.ModelForm):
 
 	class Meta:
 		model = ShippingAddress
-		fields = ['shipping_full_name', 'shipping_email', 'shipping_address', 'shipping_city', 'shipping_state', 'shipping_zipcode', 'shipping_country']
+		fields = ['shipping_full_name', 'shipping_email', 'shipping_address1','shipping_address2', 'shipping_city', 'shipping_state', 'shipping_zipcode', 'shipping_country']
 
 		exclude = ['user',]
