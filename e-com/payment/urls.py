144ca7e79f72b74c8f756908fa2e52fd49fb62b8
avijtt <infoavi9090@gmail.com>
2024-05-16 20:22:30 +0545

    formatting document

--- e-com/payment/urls.py
+++ e-com/payment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("payment-success/", views.payment_success, name = 'payment_success'),
+    path("payment-success/", views.payment_success, name = "payment_success"),
 ]
    
