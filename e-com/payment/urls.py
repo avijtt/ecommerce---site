51d08d916bb8c4597c5166e5c1441413d33c82b7
avijtt <infoavi9090@gmail.com>
2024-05-21 22:30:32 +0545

    add billing info route

--- e-com/payment/urls.py
+++ e-com/payment/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("payment-success/", views.payment_success, name = "payment_success"),
      path("checkout/", views.checkout, name = "checkout"),
+     path("billing-info/", views.billing_info, name = "billing_info"),
 ]
    
