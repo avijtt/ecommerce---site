e3d7c30b6d8bcc462c3d652d15dead90e348c76e
avijtt <infoavi9090@gmail.com>
2024-05-30 22:31:12 +0545

    add default order admin to customize the Order details

--- e-com/payment/admin.py
+++ e-com/payment/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 
+# create an orderitem inlines
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+# extend our order model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["date_order"]
+    fields = ["user", "full_name", "email", "shipping_address", "amount", "date_order", "shipped"]
+    inlines = [OrderItemInline]
+
+
+# unregister User Model
+admin.site.unregister(Order)
+
+# reregister our order and orderItem
+admin.site.register(Order, OrderAdmin)
