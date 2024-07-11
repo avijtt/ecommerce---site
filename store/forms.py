4d235827811c882f8e454aa88b0e9dceffaa78b0
avijtt <infoavi9090@gmail.com>
2024-07-11 16:08:14 +0545

    remove unwanted placeholder

--- store/forms.py
+++ store/forms.py
@@ -18,7 +18,7 @@ class SignUpForm(UserCreationForm):
 		self.fields['username'].widget.attrs['class'] = 'form-control'
 		self.fields['username'].widget.attrs['placeholder'] = 'User Name'
 		self.fields['username'].label = ''
-		self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+		self.fields['username'].help_text = '<span class="form-text text-muted">'
 
 		self.fields['password1'].widget.attrs['class'] = 'form-control'
 		self.fields['password1'].widget.attrs['placeholder'] = 'Password'
