978e933e2322131f2e181acf76de80d89c3245d3
avijtt <infoavi9090@gmail.com>
2024-07-11 16:06:47 +0545

    route for my order

--- payment/urls.py
+++ payment/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
      path("checkout/", views.checkout, name = "checkout"),
      path("billing-info/", views.billing_info, name = "billing_info"),
      path("process-order/", views.process_order ,name="process_order"),
+     path("my-order/", views.my_order ,name="my_order"),
      path("shipped-dash/", views.shipped_dash, name="shipped_dash"),
     path("not-shipped-dashdash/", views.not_shipped_dash, name="not_shipped_dash"),
     path("orders/<int:pk>/", views.orders, name="orders"),
+
 ]
    
